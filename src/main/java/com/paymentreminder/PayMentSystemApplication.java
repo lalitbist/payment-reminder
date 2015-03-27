ae0177b82bece809765c033591aadcb7979ee9c4
lalitbist <Lamait*82>
2015-03-27 15:26:36 -0700

    Consolidate ServletInitializer in one Class

--- src/main/java/com/paymentreminder/PayMentSystemApplication.java
+++ src/main/java/com/paymentreminder/PayMentSystemApplication.java
@@ -2,15 +2,22 @@ package com.paymentreminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 /**
  * 
  * @author lalit_bist
  *
  */
 @SpringBootApplication
-public class PayMentSystemApplication {
+public class PayMentSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PayMentSystemApplication.class, args);
     }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PayMentSystemApplication.class);
+	}
 }
